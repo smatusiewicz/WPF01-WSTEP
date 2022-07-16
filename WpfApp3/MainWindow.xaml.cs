d0d94ba688796b723ab58424d9c77e3662cd69f1
Dell <Dell@DESKTOP-B5UG1VL>
2022-07-16 16:24:31 +0200

    Dodaj pliki projektów.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int Counter { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Counter = 0;
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            label_content.Content = Counter++;
+        }
+    }
+}
